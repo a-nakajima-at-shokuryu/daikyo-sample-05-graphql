c66e11aa7f45c01b8b48e8a686790cf0a537867d
sakaiyuichi <44247764+sakaiyuichi@users.noreply.github.com>
2020-03-16 14:58:11 +0900

    クロスドメイン対応を追加

--- sample.js
+++ sample.js
@@ -43,6 +43,24 @@ const root = {
 }
   
 const app = express()
+
+// クロスドメイン対応
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, access_token'
+    )
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200)
+    } else {
+        next()
+    }
+}
+app.use(allowCrossDomain)
+
 app.use('/graphql', express_graphql({
     schema: schema,
     rootValue: root,
